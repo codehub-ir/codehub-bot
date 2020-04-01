b3b20bf84c732bc16a17e6b202dc2886f63bf51c
mohsen <maybeevmo@gmail.com>
2020-04-01 18:00:17 +0430

    V 1.1 main file

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ codehub = "codehub.pythonanywhere.com"
 
 from telegram.ext import CommandHandler, MessageHandler, Updater, Filters
 import requests
+from os import remove
 import func_conf, text_conf
 
 conn = func_conf.connection_gen()
 c = conn.cursor()
 func_conf.table_gen(c)
+func_conf.reset_temp_files()
 
 def start_func(update, context):
     update.message.reply_text(text_conf.start_text)
@@ -30,10 +32,39 @@ def paste_func(update, context):
     except:
         update.message.reply_text(text_conf.exception_gi)
 
+def python_doc_func(update, context):
+    update.message.document.get_file().download("./temp_file.py")
+    with open("./temp_file.py") as pff:
+        req = requests.post(text_conf.url,
+                        data=func_conf.param_gen(update.message.from_user.first_name,
+                                                 pff.read(),"python"))
+        update.message.reply_text(req.json()["link"])
+    try:
+        remove("temp_file.py")
+    except:
+        pass
+
+def text_doc_func(update, context):
+    try:
+        update.message.document.get_file().download("./temp_file_text.txt")
+        with open("./temp_file_text.txt") as tff:
+            req = requests.post(text_conf.url,
+                            data=func_conf.param_gen(update.message.from_user.first_name,
+                                                    tff.read(),"text"))
+
+            update.message.reply_text(req.json()["link"])
+        try:
+            remove("temp_file_text.txt")
+        except:
+            pass
+    except Exception as error:
+        print(error)
 
 updater = Updater(text_conf.token, use_context=True)
 updater.dispatcher.add_handler(CommandHandler('start', start_func))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, paste_func))
+updater.dispatcher.add_handler(MessageHandler(Filters.document.py, python_doc_func))
+updater.dispatcher.add_handler(MessageHandler(Filters.document.txt, text_doc_func))
 print("Running ...")
 updater.start_polling()
 updater.idle()
