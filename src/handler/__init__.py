c4a31e13841c72c50d0620b9e4572d6dcf031cf7
mmt <pycdremail@gmail.com>
2022-03-16 17:37:27 +0330

    add "norooz" to handlers

--- src/handler/__init__.py
+++ src/handler/__init__.py
@@ -2,12 +2,14 @@ from .add_paste import creator as add_paste_creator
 from .start_command import creator as start_command_creator
 from .share_paste import creator as share_paste_creator
 from .change_lang import creator as change_lang_creator
+from .norooz import creator as norooz_creator
 
 CREATORES = (
     add_paste_creator,
     start_command_creator,
     share_paste_creator,
     change_lang_creator,
+    norooz_creator,
 )
 
 def get_handlers(model):
